3f16313fd0ad496646bde02fdb60a24e1e71cdb4
Fábio Almeida <fabio.almeida@southsystem.com.br>
2021-02-05 18:49:53 -0300

    Section 43: state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,40 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p>This is really working!</p>
-          <Person />
-      </div>
-    );
-  }
+    state = {
+        persons: [
+            { name: 'Max', age: 28},
+            { name: 'Manu', age: 29},
+            { name: 'Stephanie', age: 26}
+        ],
+        otherState: 'some other value'
+    }
+
+    // Handler for mathods applied by event
+    switchNameHandler = () => {
+        // console.log('Was clicked!');
+        // DON'T DO THIS: this.state.persons[0].name = "Maximilian";
+        this.setState({
+            persons: [
+                { name: 'Maximilian', age: 28},
+                { name: 'Manu', age: 29},
+                { name: 'Stephanie', age: 27}
+            ]
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Sucha Hobbies</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+            </div>
+        );
+    }
 }
 
 export default App;
